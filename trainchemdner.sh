34fb8b01427808f775c955afd1235f649e45da1d
Fábio Maia <fabiu.maia@gmail.com>
2018-05-22 17:11:26 +0100

    Cleanup train script

--- trainchemdner.sh
+++ trainchemdner.sh
@@ -4,6 +4,5 @@
 			   -c chemdner/training.abstracts.bc2 \
 			   -a chemdner/training.annotations.bc2 \
 			   -f chemdner/model.config \
-			   -m enumerated_suffix_prefix.model \
-			   -o chemdner/ \
-               -t 4
+			   -m enumerated_suffix_prefix \
+			   -o chemdner/
