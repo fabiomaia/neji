cee6462cf4df3144e0c67ab50fa2745490e4e30d
Fábio Maia <fabiu.maia@gmail.com>
2018-06-14 15:43:38 +0100

    Fix example train script

--- example_train.sh
+++ example_train.sh
@@ -2,5 +2,5 @@
 
 ./nejiTrain.sh -if BC2 \
               -c example/train/sentences -a example/train/annotations \
-              -f example/train/model.config  \
+              -f example/train/bw_o2_windows.config  \
               -m genetag_lite_model -o example/train/
