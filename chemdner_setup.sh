758092a5d3cc4fd7406a05200f42fc43e94cd853
Fábio Maia <fabiu.maia@gmail.com>
2018-06-27 14:10:19 +0100

    Handle titles/abstracts

--- chemdner_setup.sh
+++ chemdner_setup.sh
@@ -6,7 +6,8 @@ echo "Converting training.abstracts from TSV to BC2..." && sleep 1
 rm "./chemdner/training.abstracts.bc2"
 while IFS=$'\t' read -r field1 field2 field3; do
     echo $field1
-    echo -e "$field1 $field2 $field3" >> "./chemdner/training.abstracts.bc2"
+    echo -e "$field1"T" $field2" >> "./chemdner/training.abstracts.bc2"
+    echo -e "$field1"A" $field3" >> "./chemdner/training.abstracts.bc2"
 done < ./chemdner/training.abstracts.txt
 
 ########################################################################
@@ -16,7 +17,7 @@ echo "Converting training.annotations from TSV to BC2..." && sleep 1
 rm "./chemdner/training.annotations.bc2"
 while IFS=$'\t' read -r field1 field2 field3 field4 field5 field6; do
     echo $field1
-    echo -e "$field1|$field3 $field4|$field5" >> "./chemdner/training.annotations.bc2"
+    echo -e "$field1$field2|$field3 $field4|$field5" >> "./chemdner/training.annotations.bc2"
 done < ./chemdner/training.annotations.txt
 
 ########################################################################
@@ -25,5 +26,6 @@ echo "Splitting development.abstracts from TSV to multiple RAW files..." && slee
 rm -rf "./chemdner/annotate/in/" && mkdir -p "./chemdner/annotate/in/"
 while IFS=$'\t' read -r field1 field2 field3; do
     echo $field1
-    echo -e "$field2\n$field3" > "./chemdner/annotate/in/$field1.txt"
+    echo -e "$field2" > ./chemdner/annotate/in/"$field1"T.txt
+    echo -e "$field3" > ./chemdner/annotate/in/"$field1"A.txt
 done < ./chemdner/development.abstracts.txt
