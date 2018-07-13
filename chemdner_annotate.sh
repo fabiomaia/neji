70518d3a2a9d50be056698d422562a60ebee3c3a
Fábio Maia <fabiu.maia@gmail.com>
2018-07-13 17:49:25 +0100

    Fix bugs

--- chemdner_annotate.sh
+++ chemdner_annotate.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
-rm -rf ./chemdner/annotate/out/ && mkdir -p ./chemdner/annotate/out/
-
+echo "Annotating corpus..." && sleep 1
+rm -rf "./chemdner/annotate/out/" && mkdir -p "./chemdner/annotate/out/"
 ./neji.sh -i chemdner/annotate/in/ -if RAW \
-          -o chemdner/annotate/out/ -of BC2 \
+          -o chemdner/annotate/out/ -of A1 \
           -m chemdner/enumerated_suffix_prefix/ \
           -noids
+
+echo "Building prediction file..." && sleep 1
+rm "./chemdner/predictions.txt"
+for filename in ./chemdner/annotate/out/*.a1; do
+    if [ -s "$filename" ]; then
+        echo $filename
+        id=$(basename $filename | cut -d'.' -f1 | head -c -2)
+        sentence_type=${filename: -4 : 1}
+        while IFS=$'\t' read -r field1 field2 field3; read -r tmp; do
+            startidx=$(echo $field2 | cut -d " " -f2)
+            endidx=$(echo $field2 | cut -d " " -f3)
+            echo -e "$id\t$sentence_type:$startidx:$endidx" >> ./chemdner/predictions.txt
+        done < "$filename"
+    fi
+done
--- chemdner_eval.sh
+++ chemdner_eval.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-
-echo "Building prediction file..." && sleep 1
-rm "./chemdner/enumprefix_prediction.txt"
-for filename in ./chemdner/annotate/out/*.bc2; do
-    if [ -s "$filename" ]; then
-        sentence_type=${filename: -5 : 1}
-        id=$(echo $filename | cut -d'/' -f5  | cut -d'.' -f1 | head -c -2)
-        while IFS="|" read -r field1 field2 field3; do
-            offsets=${field2// /:}
-            echo -e "$id\t$sentence_type:$offsets" >> ./chemdner/enumprefix_prediction.txt
-        done < "$filename"
-    fi
-done
+#! /bin/bash
 
 echo "Evaluating..." && sleep 1
-bc-evaluate -l "./chemdner/enumprefix_prediction.txt" "./chemdner/development.predictions.txt"
+bc-evaluate -l "./chemdner/predictions.txt" "./chemdner/development.predictions.txt"
