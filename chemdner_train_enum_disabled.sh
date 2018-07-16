2de919431b400a41a0a65ee1e5162675163c8502
Fábio Maia <fabiu.maia@gmail.com>
2018-07-16 17:41:25 +0100

    Separate scripts by enum setting

--- chemdner_train_enum_disabled.sh
+++ chemdner_train_enum_disabled.sh
@@ -5,7 +5,7 @@ rm -rf ./chemdner/enumerated_suffix_prefix/
 ./nejiTrain.sh -if BC2 \
                -c chemdner/training.abstracts.bc2 \
                -a chemdner/training.annotations.bc2 \
-               -f chemdner/model.config \
+               -f chemdner/model_enum_disabled.config \
                -m enumerated_suffix_prefix \
                -o chemdner/ \
                -t 4
