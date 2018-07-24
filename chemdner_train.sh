3374ec3a1be3a712eb788bf6cd0a70c04acfcaa9
Fábio Maia <fabiu.maia@gmail.com>
2018-07-24 12:33:01 +0100

    Remove unnecessary find&replace

--- chemdner_train.sh
+++ chemdner_train.sh
@@ -13,8 +13,6 @@ while IFS=$'\t' read -r field1 field2 field3 field4 field5 field6; do
     echo -e "TX\tCHEM\t$field3 $field4\t$field5" >> "./chemdner_corpus/train/$field1$field2.a1"
 done < ./chemdner_corpus/training.annotations.txt
 
-sed -i "s/NO CLASS/NOCLASS/g" ./chemdner_corpus/train/*.a1
-
 rm -rf ./enumerated_suffix_prefix/
 echo "Training..." && sleep 1
 ./nejiTrain.sh -if A1 \
